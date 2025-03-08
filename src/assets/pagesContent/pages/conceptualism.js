293380f36a6a0abef10050b96f4da218c9ac84a1
larisayagodina <cheeseroll@mail.ru>
2025-03-08 11:03:33 +0300

    added content

--- src/assets/pagesContent/pages/conceptualism.js
+++ src/assets/pagesContent/pages/conceptualism.js
@@ -1,50 +1,21 @@
-// import conceptualism1 from "../../images/conceptualism/conceptualism1.jpeg";
-// import conceptualism2 from "../../images/conceptualism/conceptualism2.jpeg";
-// import conceptualism3 from "../../images/conceptualism/conceptualism3.jpeg";
-// import conceptualism4 from "../../images/conceptualism/conceptualism4.jpeg";
-// import conceptualism5 from "../../images/conceptualism/conceptualism5.jpeg";
-// import conceptualism6 from "../../images/conceptualism/conceptualism6.jpeg";
-// import conceptualism7 from "../../images/conceptualism/conceptualism7.jpeg";
-// import conceptualism8 from "../../images/conceptualism/conceptualism8.jpeg";
-// import conceptualism9 from "../../images/conceptualism/conceptualism9.jpeg";
-// import conceptualism10 from "../../images/conceptualism/conceptualism10.jpeg";
-// import conceptualism11 from "../../images/conceptualism/conceptualism11.jpeg";
-// import conceptualism12 from "../../images/conceptualism/conceptualism12.jpeg";
-//
-// import conceptualism01 from "../../images/conceptualism/extra/conceptualism1.jpeg";
-// import conceptualism02 from "../../images/conceptualism/extra/conceptualism2.jpeg";
-// import conceptualism03 from "../../images/conceptualism/extra/conceptualism3.jpeg";
-// import conceptualism04 from "../../images/conceptualism/extra/conceptualism4.jpeg";
-// import conceptualism05 from "../../images/conceptualism/extra/conceptualism5.jpeg";
-// import conceptualism06 from "../../images/conceptualism/extra/conceptualism6.jpeg";
-// import conceptualism07 from "../../images/conceptualism/extra/conceptualism7.jpeg";
-// import conceptualism08 from "../../images/conceptualism/extra/conceptualism8.jpeg";
-// import conceptualism09 from "../../images/conceptualism/extra/conceptualism9.jpeg";
-// import conceptualism010 from "../../images/conceptualism/extra/conceptualism10.jpeg";
-// import conceptualism011 from "../../images/conceptualism/extra/conceptualism11.jpeg";
-// import conceptualism012 from "../../images/conceptualism/extra/conceptualism12.jpeg";
-// import conceptualism013 from "../../images/conceptualism/extra/conceptualism13.jpeg";
-// import conceptualism014 from "../../images/conceptualism/extra/conceptualism14.jpeg";
-// import conceptualism015 from "../../images/conceptualism/extra/conceptualism15.jpeg";
-// import conceptualism016 from "../../images/conceptualism/extra/conceptualism16.jpeg";
-// import conceptualism017 from "../../images/conceptualism/extra/conceptualism17.jpeg";
-// import conceptualism018 from "../../images/conceptualism/extra/conceptualism18.jpeg";
-// import conceptualism019 from "../../images/conceptualism/extra/conceptualism19.jpeg";
-// import conceptualism020 from "../../images/conceptualism/extra/conceptualism20.jpeg";
-// import conceptualism021 from "../../images/conceptualism/extra/conceptualism21.jpeg";
-// import conceptualism022 from "../../images/conceptualism/extra/conceptualism22.jpeg";
-// import conceptualism023 from "../../images/conceptualism/extra/conceptualism23.jpeg";
-// import conceptualism024 from "../../images/conceptualism/extra/conceptualism24.jpeg";
-// import conceptualism025 from "../../images/conceptualism/extra/conceptualism25.jpeg";
-// import conceptualism026 from "../../images/conceptualism/extra/conceptualism26.jpeg";
-// import conceptualism027 from "../../images/conceptualism/extra/conceptualism27.jpeg";
-// import conceptualism028 from "../../images/conceptualism/extra/conceptualism28.jpeg";
-// import conceptualism029 from "../../images/conceptualism/extra/conceptualism29.jpeg";
-// import conceptualism030 from "../../images/conceptualism/extra/conceptualism30.jpeg";
-// import conceptualism031 from "../../images/conceptualism/extra/conceptualism31.jpeg";
-// import conceptualism032 from "../../images/conceptualism/extra/conceptualism32.jpeg";
-// import conceptualism033 from "../../images/conceptualism/extra/conceptualism33.jpeg";
-
+import conceptualism1 from "../../images/conceptualism/conceptualism1.jpeg";
+import conceptualism2 from "../../images/conceptualism/conceptualism2.jpeg";
+import conceptualism3 from "../../images/conceptualism/conceptualism3.jpeg";
+import conceptualism4 from "../../images/conceptualism/conceptualism4.jpeg";
+import conceptualism5 from "../../images/conceptualism/conceptualism5.jpeg";
+import conceptualism6 from "../../images/conceptualism/conceptualism6.jpeg";
+import conceptualism7 from "../../images/conceptualism/conceptualism7.jpeg";
+import conceptualism8 from "../../images/conceptualism/conceptualism8.jpeg";
+import conceptualism9 from "../../images/conceptualism/conceptualism9.jpeg";
+import conceptualism10 from "../../images/conceptualism/conceptualism10.jpeg";
+import conceptualism11 from "../../images/conceptualism/conceptualism11.jpeg";
+import conceptualism12 from "../../images/conceptualism/conceptualism12.jpeg";
+import conceptualism13 from "../../images/conceptualism/conceptualism13.jpeg";
+import conceptualism14 from "../../images/conceptualism/conceptualism14.jpeg";
+import conceptualism15 from "../../images/conceptualism/conceptualism15.jpeg";
+import conceptualism16 from "../../images/conceptualism/conceptualism16.jpeg";
+import conceptualism17 from "../../images/conceptualism/conceptualism17.jpeg";
+import conceptualism18 from "../../images/conceptualism/conceptualism18.jpeg";
 
 export const conceptualism = {
     id: '5',
@@ -53,60 +24,28 @@ export const conceptualism = {
     blocks: [
         {
             type: 'photo',
-            title: 'Нюансы',
+            title: 'Нюансы и примеры',
             content: [
-                // conceptualism1,
-                // conceptualism2,
-                // conceptualism3,
-                // conceptualism4,
-                // conceptualism5,
-                // conceptualism6,
-                // conceptualism7,
-                // conceptualism8,
-                // conceptualism9,
-                // conceptualism10,
-                // conceptualism11,
-                // conceptualism12,
+                conceptualism1,
+                conceptualism2,
+                conceptualism3,
+                conceptualism4,
+                conceptualism5,
+                conceptualism6,
+                conceptualism7,
+                conceptualism8,
+                conceptualism9,
+                conceptualism10,
+                conceptualism11,
+                conceptualism12,
+                conceptualism13,
+                conceptualism14,
+                conceptualism15,
+                conceptualism16,
+                conceptualism17,
+                conceptualism18,
             ]
         },
-        {
-            type: 'photo',
-            title: 'Примеры',
-            content: [
-                // conceptualism01,
-                // conceptualism02,
-                // conceptualism03,
-                // conceptualism04,
-                // conceptualism05,
-                // conceptualism06,
-                // conceptualism07,
-                // conceptualism08,
-                // conceptualism09,
-                // conceptualism010,
-                // conceptualism011,
-                // conceptualism012,
-                // conceptualism013,
-                // conceptualism014,
-                // conceptualism015,
-                // conceptualism016,
-                // conceptualism017,
-                // conceptualism018,
-                // conceptualism019,
-                // conceptualism020,
-                // conceptualism021,
-                // conceptualism022,
-                // conceptualism023,
-                // conceptualism024,
-                // conceptualism025,
-                // conceptualism026,
-                // conceptualism027,
-                // conceptualism028,
-                // conceptualism029,
-                // conceptualism030,
-                // conceptualism031,
-                // conceptualism032,
-                // conceptualism033,
-            ]
-        }
+
     ],
 }
--- src/assets/pagesContent/pages/deconstructivism.js
+++ src/assets/pagesContent/pages/deconstructivism.js
@@ -1,49 +1,72 @@
-// import deconstructivism1 from "../../images/deconstructivism/deconstructivism1.jpeg";
-// import deconstructivism2 from "../../images/deconstructivism/deconstructivism2.jpeg";
-// import deconstructivism3 from "../../images/deconstructivism/deconstructivism3.jpeg";
-// import deconstructivism4 from "../../images/deconstructivism/deconstructivism4.jpeg";
-// import deconstructivism5 from "../../images/deconstructivism/deconstructivism5.jpeg";
-// import deconstructivism6 from "../../images/deconstructivism/deconstructivism6.jpeg";
-// import deconstructivism7 from "../../images/deconstructivism/deconstructivism7.jpeg";
-// import deconstructivism8 from "../../images/deconstructivism/deconstructivism8.jpeg";
-// import deconstructivism9 from "../../images/deconstructivism/deconstructivism9.jpeg";
-// import deconstructivism10 from "../../images/deconstructivism/deconstructivism10.jpeg";
-// import deconstructivism11 from "../../images/deconstructivism/deconstructivism11.jpeg";
-// import deconstructivism12 from "../../images/deconstructivism/deconstructivism12.jpeg";
-//
-// import deconstructivism01 from "../../images/deconstructivism/extra/deconstructivism1.jpeg";
-// import deconstructivism02 from "../../images/deconstructivism/extra/deconstructivism2.jpeg";
-// import deconstructivism03 from "../../images/deconstructivism/extra/deconstructivism3.jpeg";
-// import deconstructivism04 from "../../images/deconstructivism/extra/deconstructivism4.jpeg";
-// import deconstructivism05 from "../../images/deconstructivism/extra/deconstructivism5.jpeg";
-// import deconstructivism06 from "../../images/deconstructivism/extra/deconstructivism6.jpeg";
-// import deconstructivism07 from "../../images/deconstructivism/extra/deconstructivism7.jpeg";
-// import deconstructivism08 from "../../images/deconstructivism/extra/deconstructivism8.jpeg";
-// import deconstructivism09 from "../../images/deconstructivism/extra/deconstructivism9.jpeg";
-// import deconstructivism010 from "../../images/deconstructivism/extra/deconstructivism10.jpeg";
-// import deconstructivism011 from "../../images/deconstructivism/extra/deconstructivism11.jpeg";
-// import deconstructivism012 from "../../images/deconstructivism/extra/deconstructivism12.jpeg";
-// import deconstructivism013 from "../../images/deconstructivism/extra/deconstructivism13.jpeg";
-// import deconstructivism014 from "../../images/deconstructivism/extra/deconstructivism14.jpeg";
-// import deconstructivism015 from "../../images/deconstructivism/extra/deconstructivism15.jpeg";
-// import deconstructivism016 from "../../images/deconstructivism/extra/deconstructivism16.jpeg";
-// import deconstructivism017 from "../../images/deconstructivism/extra/deconstructivism17.jpeg";
-// import deconstructivism018 from "../../images/deconstructivism/extra/deconstructivism18.jpeg";
-// import deconstructivism019 from "../../images/deconstructivism/extra/deconstructivism19.jpeg";
-// import deconstructivism020 from "../../images/deconstructivism/extra/deconstructivism20.jpeg";
-// import deconstructivism021 from "../../images/deconstructivism/extra/deconstructivism21.jpeg";
-// import deconstructivism022 from "../../images/deconstructivism/extra/deconstructivism22.jpeg";
-// import deconstructivism023 from "../../images/deconstructivism/extra/deconstructivism23.jpeg";
-// import deconstructivism024 from "../../images/deconstructivism/extra/deconstructivism24.jpeg";
-// import deconstructivism025 from "../../images/deconstructivism/extra/deconstructivism25.jpeg";
-// import deconstructivism026 from "../../images/deconstructivism/extra/deconstructivism26.jpeg";
-// import deconstructivism027 from "../../images/deconstructivism/extra/deconstructivism27.jpeg";
-// import deconstructivism028 from "../../images/deconstructivism/extra/deconstructivism28.jpeg";
-// import deconstructivism029 from "../../images/deconstructivism/extra/deconstructivism29.jpeg";
-// import deconstructivism030 from "../../images/deconstructivism/extra/deconstructivism30.jpeg";
-// import deconstructivism031 from "../../images/deconstructivism/extra/deconstructivism31.jpeg";
-// import deconstructivism032 from "../../images/deconstructivism/extra/deconstructivism32.jpeg";
-// import deconstructivism033 from "../../images/deconstructivism/extra/deconstructivism33.jpeg";
+import deconstructivism1 from "../../images/deconstructivism/deconstructivism1.jpeg";
+import deconstructivism2 from "../../images/deconstructivism/deconstructivism2.jpeg";
+import deconstructivism3 from "../../images/deconstructivism/deconstructivism3.jpeg";
+import deconstructivism4 from "../../images/deconstructivism/deconstructivism4.jpeg";
+import deconstructivism5 from "../../images/deconstructivism/deconstructivism5.jpeg";
+import deconstructivism6 from "../../images/deconstructivism/deconstructivism6.jpeg";
+import deconstructivism7 from "../../images/deconstructivism/deconstructivism7.jpeg";
+import deconstructivism8 from "../../images/deconstructivism/deconstructivism8.jpeg";
+import deconstructivism9 from "../../images/deconstructivism/deconstructivism9.jpeg";
+import deconstructivism10 from "../../images/deconstructivism/deconstructivism10.jpeg";
+import deconstructivism11 from "../../images/deconstructivism/deconstructivism11.jpeg";
+import deconstructivism12 from "../../images/deconstructivism/deconstructivism12.jpeg";
+import deconstructivism13 from "../../images/deconstructivism/deconstructivism13.jpeg";
+import deconstructivism14 from "../../images/deconstructivism/deconstructivism14.jpeg";
+import deconstructivism15 from "../../images/deconstructivism/deconstructivism15.jpeg";
+import deconstructivism16 from "../../images/deconstructivism/deconstructivism16.jpeg";
+import deconstructivism17 from "../../images/deconstructivism/deconstructivism17.jpeg";
+
+import deconstructivism01 from "../../images/deconstructivism/extra/deconstructivism1.jpeg";
+import deconstructivism02 from "../../images/deconstructivism/extra/deconstructivism2.jpeg";
+import deconstructivism03 from "../../images/deconstructivism/extra/deconstructivism3.jpeg";
+import deconstructivism04 from "../../images/deconstructivism/extra/deconstructivism4.jpeg";
+import deconstructivism05 from "../../images/deconstructivism/extra/deconstructivism5.jpeg";
+import deconstructivism06 from "../../images/deconstructivism/extra/deconstructivism6.jpeg";
+import deconstructivism07 from "../../images/deconstructivism/extra/deconstructivism7.jpeg";
+import deconstructivism08 from "../../images/deconstructivism/extra/deconstructivism8.jpeg";
+import deconstructivism09 from "../../images/deconstructivism/extra/deconstructivism9.jpeg";
+import deconstructivism010 from "../../images/deconstructivism/extra/deconstructivism10.jpeg";
+import deconstructivism012 from "../../images/deconstructivism/extra/deconstructivism12.jpeg";
+import deconstructivism013 from "../../images/deconstructivism/extra/deconstructivism13.jpeg";
+import deconstructivism014 from "../../images/deconstructivism/extra/deconstructivism14.jpeg";
+import deconstructivism015 from "../../images/deconstructivism/extra/deconstructivism15.jpeg";
+import deconstructivism016 from "../../images/deconstructivism/extra/deconstructivism16.jpeg";
+import deconstructivism017 from "../../images/deconstructivism/extra/deconstructivism17.jpeg";
+import deconstructivism018 from "../../images/deconstructivism/extra/deconstructivism18.jpeg";
+import deconstructivism019 from "../../images/deconstructivism/extra/deconstructivism19.jpeg";
+import deconstructivism020 from "../../images/deconstructivism/extra/deconstructivism20.jpeg";
+import deconstructivism021 from "../../images/deconstructivism/extra/deconstructivism21.jpeg";
+import deconstructivism022 from "../../images/deconstructivism/extra/deconstructivism22.jpeg";
+import deconstructivism023 from "../../images/deconstructivism/extra/deconstructivism23.jpeg";
+import deconstructivism024 from "../../images/deconstructivism/extra/deconstructivism24.jpeg";
+import deconstructivism025 from "../../images/deconstructivism/extra/deconstructivism25.jpeg";
+import deconstructivism026 from "../../images/deconstructivism/extra/deconstructivism26.jpeg";
+import deconstructivism027 from "../../images/deconstructivism/extra/deconstructivism27.jpeg";
+import deconstructivism028 from "../../images/deconstructivism/extra/deconstructivism28.jpeg";
+import deconstructivism029 from "../../images/deconstructivism/extra/deconstructivism29.jpeg";
+import deconstructivism030 from "../../images/deconstructivism/extra/deconstructivism30.jpeg";
+import deconstructivism031 from "../../images/deconstructivism/extra/deconstructivism31.jpeg";
+import deconstructivism032 from "../../images/deconstructivism/extra/deconstructivism32.jpeg";
+import deconstructivism033 from "../../images/deconstructivism/extra/deconstructivism33.jpeg";
+import deconstructivism034 from "../../images/deconstructivism/extra/deconstructivism34.jpeg";
+import deconstructivism035 from "../../images/deconstructivism/extra/deconstructivism35.jpeg";
+import deconstructivism036 from "../../images/deconstructivism/extra/deconstructivism36.jpeg";
+import deconstructivism037 from "../../images/deconstructivism/extra/deconstructivism37.jpeg";
+import deconstructivism038 from "../../images/deconstructivism/extra/deconstructivism38.jpeg";
+import deconstructivism039 from "../../images/deconstructivism/extra/deconstructivism39.jpeg";
+import deconstructivism040 from "../../images/deconstructivism/extra/deconstructivism40.jpeg";
+import deconstructivism041 from "../../images/deconstructivism/extra/deconstructivism41.jpeg";
+import deconstructivism042 from "../../images/deconstructivism/extra/deconstructivism42.jpeg";
+import deconstructivism043 from "../../images/deconstructivism/extra/deconstructivism43.jpeg";
+import deconstructivism044 from "../../images/deconstructivism/extra/deconstructivism44.jpeg";
+import deconstructivism045 from "../../images/deconstructivism/extra/deconstructivism45.jpeg";
+import deconstructivism046 from "../../images/deconstructivism/extra/deconstructivism46.jpeg";
+import deconstructivism047 from "../../images/deconstructivism/extra/deconstructivism47.jpeg";
+import deconstructivism048 from "../../images/deconstructivism/extra/deconstructivism48.jpeg";
+import deconstructivism049 from "../../images/deconstructivism/extra/deconstructivism49.jpeg";
+import deconstructivism050 from "../../images/deconstructivism/extra/deconstructivism50.jpeg";
+import deconstructivism053 from "../../images/deconstructivism/extra/deconstructivism53.jpeg";
+import deconstructivism052 from "../../images/deconstructivism/extra/deconstructivism52.jpeg";
 
 
 export const deconstructivism = {
@@ -55,57 +78,83 @@ export const deconstructivism = {
             type: 'photo',
             title: 'Нюансы',
             content: [
-                // deconstructivism1,
-                // deconstructivism2,
-                // deconstructivism3,
-                // deconstructivism4,
-                // deconstructivism5,
-                // deconstructivism6,
-                // deconstructivism7,
-                // deconstructivism8,
-                // deconstructivism9,
-                // deconstructivism10,
-                // deconstructivism11,
-                // deconstructivism12,
+                deconstructivism1,
+                deconstructivism2,
+                deconstructivism3,
+                deconstructivism4,
+                deconstructivism5,
+                deconstructivism6,
+                deconstructivism7,
+                deconstructivism8,
+                deconstructivism9,
+                deconstructivism10,
+                deconstructivism11,
+                deconstructivism12,
+                deconstructivism13,
+                deconstructivism14,
+                deconstructivism15,
+                deconstructivism16,
+                deconstructivism17,
+
             ]
         },
         {
             type: 'photo',
             title: 'Примеры',
             content: [
-                // deconstructivism01,
-                // deconstructivism02,
-                // deconstructivism03,
-                // deconstructivism04,
-                // deconstructivism05,
-                // deconstructivism06,
-                // deconstructivism07,
-                // deconstructivism08,
-                // deconstructivism09,
-                // deconstructivism010,
-                // deconstructivism011,
-                // deconstructivism012,
-                // deconstructivism013,
-                // deconstructivism014,
-                // deconstructivism015,
-                // deconstructivism016,
-                // deconstructivism017,
-                // deconstructivism018,
-                // deconstructivism019,
-                // deconstructivism020,
-                // deconstructivism021,
-                // deconstructivism022,
-                // deconstructivism023,
-                // deconstructivism024,
-                // deconstructivism025,
-                // deconstructivism026,
-                // deconstructivism027,
-                // deconstructivism028,
-                // deconstructivism029,
-                // deconstructivism030,
-                // deconstructivism031,
-                // deconstructivism032,
-                // deconstructivism033,
+                deconstructivism01,
+                deconstructivism02,
+                deconstructivism03,
+                deconstructivism04,
+                deconstructivism05,
+                deconstructivism06,
+                deconstructivism07,
+                deconstructivism08,
+                deconstructivism09,
+                deconstructivism010,
+                deconstructivism012,
+                deconstructivism013,
+                deconstructivism014,
+                deconstructivism015,
+                deconstructivism016,
+                deconstructivism017,
+                deconstructivism018,
+
+                deconstructivism019,
+                deconstructivism020,
+                deconstructivism021,
+                deconstructivism022,
+                deconstructivism023,
+                deconstructivism024,
+                deconstructivism052,
+                deconstructivism025,
+                deconstructivism026,
+                deconstructivism027,
+                deconstructivism028,
+                deconstructivism029,
+                deconstructivism030,
+                deconstructivism031,
+                deconstructivism032,
+                deconstructivism033,
+                deconstructivism034,
+                deconstructivism035,
+                deconstructivism036,
+                deconstructivism053,
+                deconstructivism037,
+                deconstructivism038,
+                deconstructivism050,
+                deconstructivism039,
+                deconstructivism040,
+                deconstructivism041,
+                deconstructivism042,
+                deconstructivism043,
+                deconstructivism044,
+                deconstructivism045,
+                deconstructivism046,
+                deconstructivism049,
+                deconstructivism047,
+                deconstructivism048,
+
             ]
         }
     ],
