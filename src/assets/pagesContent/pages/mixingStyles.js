41ac9197cba81b83b574dda68d3fce35fa60708b
larisayagodina <cheeseroll@mail.ru>
2025-03-12 17:22:13 +0300

    added content

--- src/assets/pagesContent/pages/mixingStyles.js
+++ src/assets/pagesContent/pages/mixingStyles.js
@@ -1,44 +1,48 @@
-// import mixingStyles1 from "../../images/mixingStyles/mixingStyles1.jpeg";
-// import mixingStyles2 from "../../images/mixingStyles/mixingStyles2.jpeg";
-// import mixingStyles3 from "../../images/mixingStyles/mixingStyles3.jpeg";
-// import mixingStyles4 from "../../images/mixingStyles/mixingStyles4.jpeg";
-// import mixingStyles5 from "../../images/mixingStyles/mixingStyles5.jpeg";
-// import mixingStyles6 from "../../images/mixingStyles/mixingStyles6.jpeg";
-// import mixingStyles7 from "../../images/mixingStyles/mixingStyles7.jpeg";
-// import mixingStyles8 from "../../images/mixingStyles/mixingStyles8.jpeg";
-// import mixingStyles9 from "../../images/mixingStyles/mixingStyles9.jpeg";
-// import mixingStyles10 from "../../images/mixingStyles/mixingStyles10.jpeg";
-// import mixingStyles11 from "../../images/mixingStyles/mixingStyles11.jpeg";
-// import mixingStyles12 from "../../images/mixingStyles/mixingStyles12.jpeg";
-//
-// import mixingStyles01 from "../../images/mixingStyles/extra/mixingStyles1.jpeg";
-// import mixingStyles02 from "../../images/mixingStyles/extra/mixingStyles2.jpeg";
-// import mixingStyles03 from "../../images/mixingStyles/extra/mixingStyles3.jpeg";
-// import mixingStyles04 from "../../images/mixingStyles/extra/mixingStyles4.jpeg";
-// import mixingStyles05 from "../../images/mixingStyles/extra/mixingStyles5.jpeg";
-// import mixingStyles06 from "../../images/mixingStyles/extra/mixingStyles6.jpeg";
-// import mixingStyles07 from "../../images/mixingStyles/extra/mixingStyles7.jpeg";
-// import mixingStyles08 from "../../images/mixingStyles/extra/mixingStyles8.jpeg";
-// import mixingStyles09 from "../../images/mixingStyles/extra/mixingStyles9.jpeg";
-// import mixingStyles010 from "../../images/mixingStyles/extra/mixingStyles10.jpeg";
-// import mixingStyles011 from "../../images/mixingStyles/extra/mixingStyles11.jpeg";
-// import mixingStyles012 from "../../images/mixingStyles/extra/mixingStyles12.jpeg";
-// import mixingStyles013 from "../../images/mixingStyles/extra/mixingStyles13.jpeg";
-// import mixingStyles014 from "../../images/mixingStyles/extra/mixingStyles14.jpeg";
-// import mixingStyles015 from "../../images/mixingStyles/extra/mixingStyles15.jpeg";
-// import mixingStyles016 from "../../images/mixingStyles/extra/mixingStyles16.jpeg";
-// import mixingStyles017 from "../../images/mixingStyles/extra/mixingStyles17.jpeg";
-// import mixingStyles018 from "../../images/mixingStyles/extra/mixingStyles18.jpeg";
-// import mixingStyles019 from "../../images/mixingStyles/extra/mixingStyles19.jpeg";
-// import mixingStyles020 from "../../images/mixingStyles/extra/mixingStyles20.jpeg";
-// import mixingStyles021 from "../../images/mixingStyles/extra/mixingStyles21.jpeg";
-// import mixingStyles022 from "../../images/mixingStyles/extra/mixingStyles22.jpeg";
-// import mixingStyles023 from "../../images/mixingStyles/extra/mixingStyles23.jpeg";
-// import mixingStyles024 from "../../images/mixingStyles/extra/mixingStyles24.jpeg";
-// import mixingStyles025 from "../../images/mixingStyles/extra/mixingStyles25.jpeg";
-// import mixingStyles026 from "../../images/mixingStyles/extra/mixingStyles26.jpeg";
-// import mixingStyles027 from "../../images/mixingStyles/extra/mixingStyles27.jpeg";
-// import mixingStyles028 from "../../images/mixingStyles/extra/mixingStyles28.jpeg";
+import mixingStyles1 from "../../images/mixingStyles/mixingStyles1.jpeg";
+import mixingStyles2 from "../../images/mixingStyles/mixingStyles2.jpeg";
+import mixingStyles3 from "../../images/mixingStyles/mixingStyles3.jpeg";
+import mixingStyles4 from "../../images/mixingStyles/mixingStyles4.jpeg";
+import mixingStyles5 from "../../images/mixingStyles/mixingStyles5.jpeg";
+import mixingStyles6 from "../../images/mixingStyles/mixingStyles6.jpeg";
+import mixingStyles7 from "../../images/mixingStyles/mixingStyles7.jpeg";
+import mixingStyles8 from "../../images/mixingStyles/mixingStyles8.jpeg";
+import mixingStyles9 from "../../images/mixingStyles/mixingStyles9.jpeg";
+import mixingStyles10 from "../../images/mixingStyles/mixingStyles10.jpeg";
+import mixingStyles11 from "../../images/mixingStyles/mixingStyles11.jpeg";
+import mixingStyles12 from "../../images/mixingStyles/mixingStyles12.jpeg";
+import mixingStyles13 from "../../images/mixingStyles/mixingStyles13.jpeg";
+import mixingStyles14 from "../../images/mixingStyles/mixingStyles14.jpeg";
+import mixingStyles15 from "../../images/mixingStyles/mixingStyles15.jpeg";
+
+import mixingStyles01 from "../../images/mixingStyles/extra/mixingStyles1.jpg";
+import mixingStyles02 from "../../images/mixingStyles/extra/mixingStyles2.jpg";
+import mixingStyles03 from "../../images/mixingStyles/extra/mixingStyles3.jpg";
+import mixingStyles04 from "../../images/mixingStyles/extra/mixingStyles4.jpg";
+import mixingStyles05 from "../../images/mixingStyles/extra/mixingStyles5.jpg";
+import mixingStyles06 from "../../images/mixingStyles/extra/mixingStyles6.jpg";
+import mixingStyles07 from "../../images/mixingStyles/extra/mixingStyles7.jpg";
+import mixingStyles08 from "../../images/mixingStyles/extra/mixingStyles8.jpg";
+import mixingStyles09 from "../../images/mixingStyles/extra/mixingStyles9.jpg";
+import mixingStyles010 from "../../images/mixingStyles/extra/mixingStyles10.jpg";
+import mixingStyles011 from "../../images/mixingStyles/extra/mixingStyles11.jpg";
+import mixingStyles012 from "../../images/mixingStyles/extra/mixingStyles12.jpg";
+import mixingStyles013 from "../../images/mixingStyles/extra/mixingStyles13.jpg";
+import mixingStyles014 from "../../images/mixingStyles/extra/mixingStyles14.jpg";
+import mixingStyles015 from "../../images/mixingStyles/extra/mixingStyles15.jpg";
+import mixingStyles016 from "../../images/mixingStyles/extra/mixingStyles16.jpg";
+
+import mixingStyles017 from "../../images/mixingStyles/extra/mixingStyles17.jpeg";
+import mixingStyles018 from "../../images/mixingStyles/extra/mixingStyles18.jpeg";
+import mixingStyles019 from "../../images/mixingStyles/extra/mixingStyles19.jpeg";
+import mixingStyles020 from "../../images/mixingStyles/extra/mixingStyles20.jpeg";
+import mixingStyles021 from "../../images/mixingStyles/extra/mixingStyles21.jpeg";
+import mixingStyles022 from "../../images/mixingStyles/extra/mixingStyles22.jpeg";
+import mixingStyles023 from "../../images/mixingStyles/extra/mixingStyles23.jpeg";
+import mixingStyles024 from "../../images/mixingStyles/extra/mixingStyles24.jpeg";
+import mixingStyles025 from "../../images/mixingStyles/extra/mixingStyles25.jpeg";
+import mixingStyles026 from "../../images/mixingStyles/extra/mixingStyles26.jpeg";
+import mixingStyles027 from "../../images/mixingStyles/extra/mixingStyles27.jpeg";
+import mixingStyles028 from "../../images/mixingStyles/extra/mixingStyles28.jpeg";
 // import mixingStyles029 from "../../images/mixingStyles/extra/mixingStyles29.jpeg";
 // import mixingStyles030 from "../../images/mixingStyles/extra/mixingStyles30.jpeg";
 // import mixingStyles031 from "../../images/mixingStyles/extra/mixingStyles31.jpeg";
@@ -53,54 +57,95 @@ export const mixingStyles = {
     blocks: [
         {
             type: 'photo',
-            title: 'Нюансы',
+            title: 'Мультивещи',
             content: [
-                // mixingStyles1,
-                // mixingStyles2,
-                // mixingStyles3,
-                // mixingStyles4,
-                // mixingStyles5,
-                // mixingStyles6,
-                // mixingStyles7,
-                // mixingStyles8,
-                // mixingStyles9,
-                // mixingStyles10,
-                // mixingStyles11,
-                // mixingStyles12,
+                mixingStyles1,
+                mixingStyles2,
+                mixingStyles3,
+                mixingStyles4,
+                mixingStyles5,
+                mixingStyles6,
+                mixingStyles7,
+                mixingStyles8,
+                mixingStyles9,
+                mixingStyles10,
+                mixingStyles11,
+                mixingStyles12,
+                mixingStyles13,
+                mixingStyles14,
+                mixingStyles15,
+
             ]
         },
         {
             type: 'photo',
-            title: 'Примеры',
+            title: 'Платье',
             content: [
-                // mixingStyles01,
-                // mixingStyles02,
-                // mixingStyles03,
-                // mixingStyles04,
-                // mixingStyles05,
-                // mixingStyles06,
-                // mixingStyles07,
-                // mixingStyles08,
-                // mixingStyles09,
-                // mixingStyles010,
-                // mixingStyles011,
-                // mixingStyles012,
-                // mixingStyles013,
-                // mixingStyles014,
-                // mixingStyles015,
-                // mixingStyles016,
-                // mixingStyles017,
-                // mixingStyles018,
-                // mixingStyles019,
-                // mixingStyles020,
-                // mixingStyles021,
-                // mixingStyles022,
-                // mixingStyles023,
-                // mixingStyles024,
-                // mixingStyles025,
-                // mixingStyles026,
-                // mixingStyles027,
-                // mixingStyles028,
+                mixingStyles01,
+                mixingStyles02,
+            ]
+        },
+        {
+            type: 'photo',
+            title: 'Пиджак',
+            content: [
+                mixingStyles03,
+                mixingStyles04,
+                mixingStyles05,
+            ]
+        },
+        {
+            type: 'photo',
+            title: 'Платье',
+            content: [
+                mixingStyles06,
+                mixingStyles07,
+                mixingStyles08,
+            ]
+        },
+        {
+            type: 'photo',
+            title: 'Рубашка',
+            content: [
+                mixingStyles09,
+                mixingStyles010,
+                mixingStyles011,
+            ]
+        },
+        {
+            type: 'photo',
+            title: 'Топ',
+            content: [
+                mixingStyles012,
+                mixingStyles013,
+                mixingStyles014,
+            ]
+        },
+        {
+            type: 'photo',
+            title: 'Примеры базы + стиля',
+            content: [
+                mixingStyles015,
+                mixingStyles016,
+            ]
+        },
+
+        {
+            type: 'photo',
+            title: 'Как миксовать 2-3 стиля',
+            content: [
+                mixingStyles017,
+                mixingStyles018,
+                mixingStyles019,
+                mixingStyles020,
+                mixingStyles021,
+                mixingStyles022,
+                mixingStyles023,
+                mixingStyles024,
+                mixingStyles025,
+                mixingStyles026,
+                mixingStyles027,
+                mixingStyles028,
                 // mixingStyles029,
                 // mixingStyles030,
                 // mixingStyles031,
